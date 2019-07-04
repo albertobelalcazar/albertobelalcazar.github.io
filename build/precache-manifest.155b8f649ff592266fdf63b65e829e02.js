7dc955a3043b4572f52e55d545e565540c9b9a2f
Alberto Belalcazar <alberto.belalcazar@hotmail.com>
2019-07-04 12:26:14 +0200

    changes on package json

---
+++ build/precache-manifest.155b8f649ff592266fdf63b65e829e02.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9713a46c1eded5ee9c659880c522f15c",
+    "url": "albertobelalcazar.github.io/index.html"
+  },
+  {
+    "revision": "86f9f657bab448bdbadb",
+    "url": "albertobelalcazar.github.io/static/css/2.aafcfe95.chunk.css"
+  },
+  {
+    "revision": "fe79a535a89a48b94d2e",
+    "url": "albertobelalcazar.github.io/static/css/main.d8a0937d.chunk.css"
+  },
+  {
+    "revision": "86f9f657bab448bdbadb",
+    "url": "albertobelalcazar.github.io/static/js/2.8606d566.chunk.js"
+  },
+  {
+    "revision": "fe79a535a89a48b94d2e",
+    "url": "albertobelalcazar.github.io/static/js/main.bd7a8fad.chunk.js"
+  },
+  {
+    "revision": "8d3a886b6823574fe22b",
+    "url": "albertobelalcazar.github.io/static/js/runtime~main.b190d3c0.js"
+  },
+  {
+    "revision": "b3ad973d70a95762cade16aa63caeec3",
+    "url": "albertobelalcazar.github.io/static/media/close-envelope.b3ad973d.svg"
+  },
+  {
+    "revision": "d42fb582a099ace3e72247546117cbc7",
+    "url": "albertobelalcazar.github.io/static/media/github-logo.d42fb582.svg"
+  },
+  {
+    "revision": "7c4e3ad6be5779d06ab356221fdeee43",
+    "url": "albertobelalcazar.github.io/static/media/linkedin.7c4e3ad6.svg"
+  },
+  {
+    "revision": "ffe3e9a276db5f1f0cc51439f8366874",
+    "url": "albertobelalcazar.github.io/static/media/sms.ffe3e9a2.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d7d8e4bdc7faf167e6e8d7c7c5fc41d3.js"
+  "albertobelalcazar.github.io/precache-manifest.155b8f649ff592266fdf63b65e829e02.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("albertobelalcazar.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
