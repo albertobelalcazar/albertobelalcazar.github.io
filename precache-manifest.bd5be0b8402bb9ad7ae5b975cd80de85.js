d7414fd67bb0233df08d2d38321035ef3fd6d2e3
Alberto Belalcazar <alberto.belalcazar@hotmail.com>
2019-07-04 12:30:51 +0200

    Updates

---
+++ precache-manifest.bd5be0b8402bb9ad7ae5b975cd80de85.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ba7ca3057345cf73de4bacc065114b",
+    "url": "albertobelalcazar.github.io/index.html"
+  },
+  {
+    "revision": "b15e15bc4567d91cefe8",
+    "url": "albertobelalcazar.github.io/static/css/2.ae6410f9.chunk.css"
+  },
+  {
+    "revision": "9b6d00fd7251eed75b2b",
+    "url": "albertobelalcazar.github.io/static/css/main.9a383ca5.chunk.css"
+  },
+  {
+    "revision": "b15e15bc4567d91cefe8",
+    "url": "albertobelalcazar.github.io/static/js/2.8606d566.chunk.js"
+  },
+  {
+    "revision": "9b6d00fd7251eed75b2b",
+    "url": "albertobelalcazar.github.io/static/js/main.98b38e2d.chunk.js"
+  },
+  {
+    "revision": "8d3a886b6823574fe22b",
+    "url": "albertobelalcazar.github.io/static/js/runtime~main.b190d3c0.js"
+  },
+  {
+    "revision": "b3ad973d70a95762cade16aa63caeec3",
+    "url": "albertobelalcazar.github.io/static/media/close-envelope.b3ad973d.svg"
+  },
+  {
+    "revision": "d42fb582a099ace3e72247546117cbc7",
+    "url": "albertobelalcazar.github.io/static/media/github-logo.d42fb582.svg"
+  },
+  {
+    "revision": "7c4e3ad6be5779d06ab356221fdeee43",
+    "url": "albertobelalcazar.github.io/static/media/linkedin.7c4e3ad6.svg"
+  },
+  {
+    "revision": "ffe3e9a276db5f1f0cc51439f8366874",
+    "url": "albertobelalcazar.github.io/static/media/sms.ffe3e9a2.svg"
+  }
+]);
